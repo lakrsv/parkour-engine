cff59add3d48be29d457dbec154a4d827d47f8e3
Lars-Kristian Svenoy <lakrsv@gmail.com>
2024-04-10 15:14:38 +0100

    Remove commented code

--- engine/query_test.go
+++ engine/query_test.go
@@ -17,9 +17,6 @@ func TestMatcherMatch(t *testing.T) {
 	typeA := reflect.TypeOf(ComponentA{})
 	typeB := reflect.TypeOf(ComponentB{})
 	typeC := reflect.TypeOf(ComponentC{})
-	//storage.registerComponent(typeA)
-	//storage.registerComponent(typeB)
-	//storage.registerComponent(typeC)
 
 	// Add some entities to storage
 	storage.createEntity(ComponentA{})
