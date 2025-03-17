77eb529029e45dd07ac19239a6b59e45e8f93d90
Lars-Kristian Svenoy <lakrsv@gmail.com>
2025-03-17 17:53:05 +0000

    Remove term dependency

--- game/main.go
+++ game/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"embed"
-	"golang.org/x/term"
-	"os"
 )
 
 //go:embed assets/*
@@ -12,10 +10,5 @@ var content embed.FS
 func main() {
 	InitAudio()
 	go PlayBackgroundMusic()
-	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
-	if err != nil {
-		panic(err)
-	}
-	defer term.Restore(int(os.Stdin.Fd()), oldState)
 	Run(0)
 }
